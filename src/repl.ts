6f9026c2928a7ac39aa9ed7f85df58d9c70808b1
jkk290 <jared@kuromoto.dev>
2026-03-03 13:08:05 -1000

    Setup initial REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from "node:readline";
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+
+    rl.prompt();
+    rl.on("line", async (input) => {
+        if (!input) {
+            rl.prompt();
+            return;
+        }
+        const words = cleanInput(input);
+        console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    });
+}
+
 export function cleanInput(input: string): string[] {
     return input
         .trim()
