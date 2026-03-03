27d762b0dfd4f2d3c376c0ef4cea928a08cd6a66
jkk290 <jared@kuromoto.dev>
2026-03-03 12:20:38 -1000

    Add function for sanitizing input and create unit tests

---
+++ src/repl.test.ts
@@ -0,0 +1,33 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+    {
+        input: "  hello world ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: " hi     ",
+        expected: ["hi"],
+    },
+    {
+        input: "three short   wORdS",
+        expected: ["three", "short", "words"],
+    },
+    {
+        input: "",
+        expected: [],
+    },
+    {
+        input: "    ",
+        expected: [],
+    },
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input);
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
